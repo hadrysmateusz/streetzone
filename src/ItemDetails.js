265cb3fe6f3e8ae0df47444121e2d50359887952
ematte <kugot999@gmail.com>
2018-10-26 21:38:54 +0200

    added working details view

--- src/ItemDetails.js
+++ src/ItemDetails.js
@@ -1,36 +1,68 @@
 import React, { Component } from "react"
-import { API } from "aws-amplify"
+import { API, Auth } from "aws-amplify"
 
 export default class ItemDetails extends Component {
 	state = {
-		item: {}
+		item: null,
+		userIsOwner: false,
+		isLoading: true
 	}
-	componentDidMount = () => {
-		console.log(this.props)
-		let itemId = this.props.match.params.id
-		let item = API.get("items", `items/${itemId}`)
-		this.setState({ item })
+	componentDidMount = async () => {
+		try {
+			// get the item
+			let itemId = this.props.match.params.id
+			let item = await API.get("items", `/items/${itemId}`)
+			this.setState({ item })
+
+			// check if current user is the owner
+			let { isAuthenticated } = this.props
+			if (isAuthenticated) {
+				let info = await Auth.currentUserInfo()
+				let userIsOwner = isAuthenticated && info.id === item.userId
+				this.setState({ userIsOwner })
+			}
+		} catch (e) {
+			console.log("Authorization error")
+		}
+		this.setState({ isLoading: false })
+	}
+
+	deleteItem = async () => {
+		try {
+			let itemId = this.props.match.params.id
+			await API.del("items", `/items/${itemId}`)
+		} catch (e) {
+			alert("Usuwanie nie powiodło się")
+		}
 	}
 
 	render() {
-		const item = this.state.item
+		const { item, userIsOwner } = this.state
+
 		return (
 			<div className="ItemDetails">
-				<h3>ItemDetails</h3>
-				<div>
-					<h4>
-						{item.name}
-						<span style={{ color: "#adadad" }}>
-							{" "}
-							- {item.price}
-							zł
-						</span>
-					</h4>
-					<div style={{ fontSize: "18px", fontWeight: "bold" }}>
-						{item.designers.join(" & ")}
+				<h2>ItemDetails</h2>
+				{item && (
+					<div>
+						<h3>
+							{item.name}
+							<span style={{ color: "#adadad" }}>
+								{" "}
+								- {item.price}
+								zł
+							</span>
+						</h3>
+						<h4 style={{ color: "#3f3f3f" }}>{item.designers.join(" & ")}</h4>
+						<h4 style={{ color: "#999" }}>{item.category}</h4>
+						<p>{item.description}</p>
+						{userIsOwner && (
+							<>
+								<button>Edytuj</button>
+								<button onClick={this.deleteItem}>Usuń</button>
+							</>
+						)}
 					</div>
-					<p>{item.description}</p>
-				</div>
+				)}
 			</div>
 		)
 	}
