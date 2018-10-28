86eeb9fbd7c8f5d74f7ab1a54bff3dd21eb82379
ematte <kugot999@gmail.com>
2018-10-28 02:37:59 +0200

    updated config

--- src/awsConfig.js
+++ src/awsConfig.js
@@ -2,13 +2,13 @@ const stages = {
 	dev: {
 		apiGateway: {
 			REGION: "eu-west-1",
-			URL: "https://rp8yf3el0i.execute-api.eu-west-1.amazonaws.com/dev"
+			URL: "https://ey9b5u8yhb.execute-api.eu-west-1.amazonaws.com/dev"
 		},
 		cognito: {
 			REGION: "eu-west-1",
-			USER_POOL_ID: "eu-west-1_CXtkphY5i",
-			APP_CLIENT_ID: "5l7nk63cko8u4rbja9b3urs1ij",
-			IDENTITY_POOL_ID: "eu-west-1:b47423cf-37d0-4248-9cc4-46cfb1d1febc"
+			USER_POOL_ID: "eu-west-1_WzEFM9ETN",
+			APP_CLIENT_ID: "2cjlq90nc7bulka8c2ko81ae5k",
+			IDENTITY_POOL_ID: "eu-west-1:aac3a416-27da-421b-ac93-8ec6ce83fb04"
 		}
 	},
 
