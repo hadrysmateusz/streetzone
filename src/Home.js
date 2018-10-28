85c8b05117a17a591c7cd8696af9b589c69bd0d5
ematte <kugot999@gmail.com>
2018-10-28 02:36:57 +0200

    added existence check and minor tweaks

--- src/Home.js
+++ src/Home.js
@@ -4,40 +4,44 @@ import { Link } from "react-router-dom"
 
 export default class Home extends React.Component {
 	state = {
-		items: []
+		items: null
 	}
 	componentDidMount = async () => {
 		let data = await API.get("items", "/items")
-		this.setState({ items: data.Items })
+		this.setState({ items: data.items })
 	}
 
 	render() {
+		const { items } = this.state
 		return (
 			<div>
 				<h3>Home</h3>
-				{this.state.items.map((item) => (
-					<div key={item.itemId}>
-						<Link to={"/i/" + item.itemId}>
-							<div>
-								<h4>
-									{item.name}
-									<span style={{ color: "#adadad" }}>
-										{" "}
-										- {item.price}
-										zł
-									</span>
-								</h4>
-								<div style={{ fontSize: "18px", fontWeight: "bold" }}>
-									{item.designers.join(" & ")}
+				{/* TODO: prawidłowa odmiana 'przedmiotów' */}
+				{items && <h4>{items.length} Wyniki</h4>}
+				{items &&
+					items.map((item) => (
+						<div key={item.itemId}>
+							<Link to={"/i/" + item.itemId}>
+								<div>
+									<h4>
+										{item.name}
+										<span style={{ color: "#adadad" }}>
+											{" "}
+											- {item.price}
+											zł
+										</span>
+									</h4>
+									<div style={{ fontSize: "18px", fontWeight: "bold" }}>
+										{item.designers.join(" & ")}
+									</div>
+									<div style={{ fontSize: "15px", fontWeight: "bold" }}>
+										{new Date(item.createdAt).toLocaleString()}
+									</div>
+									<p>{item.description}</p>
 								</div>
-								<div style={{ fontSize: "15px", fontWeight: "bold" }}>
-									{"Dodano: " + new Date(item.createdAt).toLocaleString()}
-								</div>
-								<p>{item.description}</p>
-							</div>
-						</Link>
-					</div>
-				))}
+							</Link>
+						</div>
+					))}
 			</div>
 		)
 	}
