0b4392c07e1a87158010501fc3d8db2683607fea
ematte <kugot999@gmail.com>
2019-01-08 21:28:50 +0100

    new separator component

---
+++ src/components/Separator/index.js
@@ -0,0 +1,33 @@
+import React from "react"
+import styled from "styled-components"
+
+const SeparatorBase = ({ children, ...rest }) => (
+	<div {...rest}>
+		<div className="horizontal-rule" />
+		<div className="children">{children}</div>
+		<div className="horizontal-rule" />
+	</div>
+)
+
+const Separator = styled(SeparatorBase)`
+	padding: ${(p) => p.spacing || "6px"} 0;
+	display: flex;
+	align-items: center;
+	justify-content: center;
+
+	.horizontal-rule {
+		height: 1px;
+		width: 100%;
+		background: ${(p) => p.theme.colors.gray[50]};
+	}
+	.children {
+		display: grid;
+		grid-auto-flow: column;
+		align-items: center;
+		gap: 6px;
+		padding: 0 6px;
+		color: ${(p) => p.theme.colors.gray[25]};
+	}
+`
+
+export default Separator
