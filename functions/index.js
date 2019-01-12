9210dee389ae1e6750582719f14f902d57fb2552
ematte <kugot999@gmail.com>
2019-01-12 22:57:56 +0100

    new functions for user cleanup PART2

--- functions/index.js
+++ functions/index.js
@@ -1,15 +1,22 @@
 const functions = require("firebase-functions")
 const algoliasearch = require("algoliasearch")
+const admin = require("firebase-admin")
+
+var serviceAccount = require("./firebase-admin-key.json")
+
+admin.initializeApp({
+	credential: admin.credential.cert(serviceAccount),
+	databaseURL: "https://streetwear-app.firebaseio.com"
+})
 
 const ALGOLIA_ID = functions.config().algolia.app_id
 const ALGOLIA_ADMIN_KEY = functions.config().algolia.api_key
 
 const ALGOLIA_INDEX_NAME = "dev_items"
-const FIRESTORE_DOCUMENT_NAME = "items"
 const client = algoliasearch(ALGOLIA_ID, ALGOLIA_ADMIN_KEY)
 
 exports.onItemCreated = functions.firestore
-	.document(`${FIRESTORE_DOCUMENT_NAME}/{itemId}`)
+	.document(`items/{itemId}`)
 	.onCreate((snap, context) => {
 		// Get the item document
 		const item = snap.data()
@@ -23,7 +30,7 @@ exports.onItemCreated = functions.firestore
 	})
 
 exports.onItemUpdated = functions.firestore
-	.document(`${FIRESTORE_DOCUMENT_NAME}/{itemId}`)
+	.document(`items/{itemId}`)
 	.onUpdate((snap, context) => {
 		// Get the item document
 		const item = snap.after.data()
@@ -37,7 +44,7 @@ exports.onItemUpdated = functions.firestore
 	})
 
 exports.onItemDeleted = functions.firestore
-	.document(`${FIRESTORE_DOCUMENT_NAME}/{itemId}`)
+	.document(`items/{itemId}`)
 	.onDelete((snap, context) => {
 		// Get the object id
 		const objectID = context.params.itemId
@@ -46,3 +53,49 @@ exports.onItemDeleted = functions.firestore
 		const index = client.initIndex(ALGOLIA_INDEX_NAME)
 		return index.deleteObject(objectID)
 	})
+
+// exports.removeItemImages = functions.firestore
+// 	.document(`items/{itemId}`)
+// 	.onDelete((snap, context) => {
+// 		console.log("snap", snap)
+// 		console.log("snap data", snap.data())
+// 	})
+
+exports.removeUserItems = functions.firestore
+	.document(`users/{userId}`)
+	.onDelete((snap, context) => {
+		console.log("snap", snap)
+		console.log("context", context)
+		const db = admin.firestore()
+		const data = snap.data()
+		console.log("data", data)
+		console.log("items", data.items)
+		data.items.forEach((item) => {
+			db.collection("items")
+				.doc(item)
+				.delete()
+				.then(() => {
+					console.log(`deleted ${item}`)
+				})
+				.catch((err) => console.log(err))
+		})
+	})
+
+exports.userDbCleanup = functions.auth.user().onDelete((user, ...rest) => {
+	console.log("user", user)
+	console.log("rest", ...rest)
+	const userId = user.uid
+	const db = admin.firestore()
+	console.log("userSnap", db.collection("users").doc(userId))
+	db.collection("users")
+		.doc(userId)
+		.delete()
+		.then(() => {
+			console.log(`user ${userId} was deleted`)
+		})
+		.catch((err) => console.log(err))
+})
+
+exports.onUserCreated = functions.auth.user().onCreate((...args) => {
+	console.log("args", args)
+})
