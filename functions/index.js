9ab0badbbd42d14077f77d1085317d620e368ee8
ematte <kugot999@gmail.com>
2019-01-17 20:48:02 +0100

    refactor imageProcessing function

--- functions/index.js
+++ functions/index.js
@@ -6,13 +6,17 @@ const spawn = require("child-process-promise").spawn
 const path = require("path")
 const os = require("os")
 const fs = require("fs")
-const { Storage } = require("@google-cloud/storage")
 const serviceAccount = require("./firebase-admin-key.json")
 
 const JPEG_EXTENSION = ".jpg"
-const S_THUMB_POSTFIX = "_THUMB_S"
-const M_THUMB_POSTFIX = "_THUMB_M"
-const L_THUMB_POSTFIX = "_THUMB_L"
+const S_THUMB_POSTFIX = "_S_THUMB"
+const M_THUMB_POSTFIX = "_M_THUMB"
+const L_THUMB_POSTFIX = "_L_THUMB"
+
+const signedURLConfig = {
+	action: "read",
+	expires: "03-01-2500"
+}
 
 const GCP_STORAGE_BUCKET = "streetwear-app.appspot.com"
 const DATABASE_URL = "https://streetwear-app.firebaseio.com"
@@ -157,27 +161,43 @@ exports.onUserCreated = functions.auth.user().onCreate((...args) => {
 // ---------- Image handling ----------
 // ------------------------------------
 
-// On image upload create convert image to JPEG and create thumbnails
-// to save on bandwith and improve performance
-exports.imageToJPG = functions.storage.object().onFinalize((object) => {
-	// Creates a client
-	const storage = new Storage()
+const generateThumbnail = (size, pathFrom, pathTo) => {
+	return spawn("convert", [pathFrom, "-thumbnail", size, pathTo], {
+		capture: ["stdout", "stderr"]
+	})
+}
 
+const uploadThumbnail = async (file, path) => {
+	await bucket.upload(file, {
+		destination: path,
+		contentType: "image/jpeg",
+		public: true,
+		gzip: true,
+		metadata: {
+			cacheControl: "public, max-age=31536000"
+		}
+	})
+	return bucket.file(path).getSignedUrl(signedURLConfig)
+}
+
+const processImage = async (object, sizes) => {
 	const filePath = object.name
+
+	if (filePath.endsWith("THUMB")) {
+		console.log("File is already a thumbnail")
+		return false
+	}
+
 	const baseFileName = path.basename(filePath, path.extname(filePath))
 	const fileDir = path.dirname(filePath)
 	const tempLocalFile = path.join(os.tmpdir(), filePath)
 	const tempLocalDir = path.dirname(tempLocalFile)
-	// To enable Client-side caching you can set the Cache-Control headers here.
-	const metadata = {
-		"Cache-Control": "public,max-age=3600"
-	}
 
 	// Temp JPEG
 	const JPEGFilePath = path.normalize(
 		path.format({ dir: fileDir, name: baseFileName, ext: JPEG_EXTENSION })
 	)
-	const tempLocalJPEGFile = path.join(os.tmpdir(), JPEGFilePath)
+	let tempLocalJPEGFile = path.join(os.tmpdir(), JPEGFilePath)
 
 	// Temp Small Thumbnail
 	const thumbSPath = path.normalize(
@@ -199,112 +219,83 @@ exports.imageToJPG = functions.storage.object().onFinalize((object) => {
 
 	// Exit if this is triggered on a file that is not an image.
 	if (!object.contentType.startsWith("image/")) {
-		console.log("This is not an image.")
+		console.log(`This file is not an image (${object.contentType}`)
 		return null
 	}
 
-	// // Exit if the image is already a JPEG.
-	// if (object.contentType.startsWith("image/jpeg")) {
-	// 	console.log("Already a JPEG.")
-	// 	return null
-	// }
+	// Create the temp directory where the storage file will be downloaded
+	await mkdirp(tempLocalDir)
 
-	// Get the reference to the image's bucket
-	const bucket = storage.bucket(object.bucket)
-	console.log("object bucket", object.bucket)
-	console.log("bucket", bucket)
-	console.log("filepath", filePath)
+	// Download file from bucket
+	await bucket.file(filePath).download({ destination: tempLocalFile })
 
-	// Create the temp directory where the storage file will be downloaded.
-	return mkdirp(tempLocalDir)
-		.then(() => {
-			// Download file from bucket.
-			return bucket.file(filePath).download({ destination: tempLocalFile })
-		})
-		.then(() => {
-			console.log("The file has been downloaded to", tempLocalFile)
-			// Skip if the image is already a JPEG.
-			if (object.contentType.startsWith("image/jpeg")) {
-				console.log("Already a JPEG.")
-				return null
-			}
-			// Convert the image to JPEG using ImageMagick.
-			return spawn("convert", [tempLocalFile, tempLocalJPEGFile])
-		})
-		.then(() => {
-			console.log("JPEG image created at", tempLocalJPEGFile)
-			// Skip if the image is already a JPEG.
-			if (object.contentType.startsWith("image/jpeg")) {
-				console.log("Already a JPEG.")
-				return null
-			}
-			// Removing the original file
-			return bucket.file(filePath).delete()
-		})
-		.then(() => {
-			console.log("Old image removed from", filePath)
-			// Uploading the JPEG image.
-			return bucket.upload(tempLocalJPEGFile, {
-				destination: filePath,
-				metadata: metadata
-			})
-		})
-		.then(() => {
-			console.log("JPEG image uploaded to Storage at", filePath)
-			// Generate a small thumbnail using ImageMagick.
-			return spawn(
-				"convert",
-				[tempLocalJPEGFile, "-thumbnail", `100x100`, tempLocalThumbSFile],
-				{ capture: ["stdout", "stderr"] }
-			)
-		})
-		.then(() => {
-			console.log("Small thumbnail generated at", thumbSPath)
-			// Uploading the small thumbnail.
-			return bucket.upload(tempLocalThumbSFile, {
-				destination: thumbSPath,
-				metadata: metadata
-			})
-		})
-		.then(() => {
-			// Generate a medium thumbnail using ImageMagick.
-			return spawn(
-				"convert",
-				[tempLocalJPEGFile, "-thumbnail", `250x250`, tempLocalThumbMFile],
-				{ capture: ["stdout", "stderr"] }
-			)
-		})
-		.then(() => {
-			console.log("Medium thumbnail generated at", thumbMPath)
-			// Uploading the medium thumbnail.
-			return bucket.upload(tempLocalThumbMFile, {
-				destination: thumbMPath,
-				metadata: metadata
-			})
-		})
-		.then(() => {
-			// Generate a large thumbnail using ImageMagick.
-			return spawn(
-				"convert",
-				[tempLocalJPEGFile, "-thumbnail", `600x600`, tempLocalThumbLFile],
-				{ capture: ["stdout", "stderr"] }
-			)
-		})
-		.then(() => {
-			console.log("Large thumbnail generated at", thumbLPath)
-			// Uploading the large thumbnail.
-			return bucket.upload(tempLocalThumbLFile, {
-				destination: thumbLPath,
-				metadata: metadata
-			})
-		})
+	// Only convert files that aren't already a JPEG
+	if (!object.contentType.startsWith("image/jpeg")) {
+		// Convert the image to JPEG using ImageMagick.
+		await spawn("convert", [tempLocalFile, tempLocalJPEGFile])
 
-		.then(() => {
-			// Once the image has been converted delete the local files to free up disk space.
-			fs.unlinkSync(tempLocalJPEGFile)
-			fs.unlinkSync(tempLocalFile)
-			fs.unlinkSync(tempLocalThumbSFile)
-			fs.unlinkSync(tempLocalThumbLFile)
-			return
-		})
+		// Remove the original file
+		await bucket.file(filePath).delete()
+
+		// Upload the JPEG image (To the location of the original file)
+		await bucket.upload(tempLocalJPEGFile, { destination: filePath })
+	} else {
+		// If the file is already a jpeg point the JPEGFile to its location
+		tempLocalJPEGFile = tempLocalFile
+	}
+
+	// Generate thumbnails using ImageMagick
+	await generateThumbnail(sizes[0], tempLocalJPEGFile, tempLocalThumbSFile)
+	await generateThumbnail(sizes[1], tempLocalJPEGFile, tempLocalThumbMFile)
+	await generateThumbnail(sizes[2], tempLocalJPEGFile, tempLocalThumbLFile)
+
+	// Upload the thumbnails
+	const signedURLs = await Promise.all([
+		bucket.file(filePath).getSignedUrl(signedURLConfig),
+		uploadThumbnail(tempLocalThumbSFile, thumbSPath),
+		uploadThumbnail(tempLocalThumbMFile, thumbMPath),
+		uploadThumbnail(tempLocalThumbLFile, thumbLPath)
+	])
+
+	// Once the image has been converted delete the local files to free up disk space.
+	// "tempLocalFile" is not unlinked as the "tempLocalJPEGFile" is saved to that same location
+	fs.unlinkSync(tempLocalJPEGFile)
+	fs.unlinkSync(tempLocalThumbSFile)
+	fs.unlinkSync(tempLocalThumbMFile)
+	fs.unlinkSync(tempLocalThumbLFile)
+
+	return signedURLs
+}
+
+const writeUrlsToDb = async (userId, urls) => {
+	const profilePictureURLs = urls.map((result, i) => {
+		console.log(`url ${i}:`, result)
+		return result[0]
+	})
+
+	// Add the URLs to the Database
+	const db = admin.firestore()
+	return db
+		.collection("users")
+		.doc(userId)
+		.update({ profilePictureURLs })
+}
+
+// On image upload create convert image to JPEG and create thumbnails
+// to save on bandwith and improve performance
+exports.processImage = functions.storage.object().onFinalize(async (file) => {
+	if (file.name.includes("attachments/")) {
+		console.log("Processing an attachment...")
+		return await processImage(file, ["110x110", "290x290", "600x600"])
+	} else if (file.name.includes("profile-pictures/")) {
+		console.log("Processing a profile picture...")
+		const urls = await processImage(file, ["40x40", "110x110", "230x230"])
+		console.log("urls: ", urls)
+		if (urls) {
+			const userId = file.name.split("/")[1]
+			console.log("userId", userId)
+			await writeUrlsToDb(userId, urls)
+		}
+		return
+	}
 })
