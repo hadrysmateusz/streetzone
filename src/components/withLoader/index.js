c03a2a530b30713e92bfe41526a5c4e59e0117cb
ematte <kugot999@gmail.com>
2019-01-21 23:06:03 +0100

    removed the unused withLoader

--- src/components/withLoader/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import LoadingSpinner from "../LoadingSpinner"
-
-const withLoader = (C) => (props) => {
-	return !props.isLoading ? <C {...props} /> : <LoadingSpinner />
-}
-
-export default withLoader
