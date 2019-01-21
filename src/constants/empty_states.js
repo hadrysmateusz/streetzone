317aa72f2f69a89a820974d416eef8dcfb998107
ematte <kugot999@gmail.com>
2019-01-21 23:02:59 +0100

    move empty_state images to separate folder

--- src/constants/empty_states.js
+++ src/constants/empty_states.js
@@ -1,34 +1,36 @@
+const path = "/img/empty_states/"
+
 export const UserNoItems = {
 	text: "Ten użytkownik nie ma aktualnie żadnych przedmiotów na sprzedaż",
-	image: process.env.PUBLIC_URL + "/img/SadFace.png"
+	image: process.env.PUBLIC_URL + path + "SadFace.png"
 }
 
 export const UserNoFeedback = {
 	text: "Ten użytkownik nie ma jeszcze żadnych opinii",
-	image: process.env.PUBLIC_URL + "/img/SadFace.png"
+	image: process.env.PUBLIC_URL + path + "SadFace.png"
 }
 
 export const UserNoFollowing = {
 	text: "Nie obserwujesz jeszcze nikogo",
-	image: process.env.PUBLIC_URL + "/img/SadFace.png"
+	image: process.env.PUBLIC_URL + path + "SadFace.png"
 }
 
 export const UserNoLiked = {
 	text: "Nie zapisałeś jeszcze żadnego przedmiotu",
-	image: process.env.PUBLIC_URL + "/img/SadFace.png"
+	image: process.env.PUBLIC_URL + path + "SadFace.png"
 }
 
 export const UserNoSoldItems = {
 	text: "Ten użytkownik nie sprzedał jeszcze żadnego przedmiotu",
-	image: process.env.PUBLIC_URL + "/img/SadFace.png"
+	image: process.env.PUBLIC_URL + path + "SadFace.png"
 }
 
 export const NoMoreItems = {
 	text: "To już wszystko",
-	image: process.env.PUBLIC_URL + "/img/SadFace.png"
+	image: process.env.PUBLIC_URL + path + "SadFace.png"
 }
 
 export const Generic = {
 	text: "Coś poszło nie tak",
-	image: process.env.PUBLIC_URL + "/img/SadFace.png"
+	image: process.env.PUBLIC_URL + path + "SadFace.png"
 }
