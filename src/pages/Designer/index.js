eca1911c9b1802827ef924d0e3fe77506b717555
Mateusz Hadryś <kugot999@gmail.com>
2019-03-24 15:48:51 +0100

    create VERY rudimentary designers page component

--- src/pages/Designer/index.js
+++ src/pages/Designer/index.js
@@ -1,7 +1,25 @@
-import React from "react"
+import React, { useState, useEffect } from "react"
+import withRouter from "react-router/withRouter"
+import { ROUTES } from "../../constants"
 
-const DesignerPage = () => {
-	return <div />
+const createURL = (state) => `${ROUTES.MARKETPLACE}?search=${btoa(JSON.stringify(state))}`
+
+const DesignerPage = ({ match, history }) => {
+	const [value, setValue] = useState("")
+
+	useEffect(() => {
+		setValue(match.params.id)
+	})
+
+	const onClick = async () => {
+		history.push(createURL({ designers: [value] }))
+	}
+
+	return (
+		<div>
+			<button onClick={onClick}>GO</button>
+		</div>
+	)
 }
 
-export default DesignerPage
+export default withRouter(DesignerPage)
