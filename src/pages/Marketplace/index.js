c0a6cf89be72aa397c540c8ceeb5d1955caf1a5a
Mateusz Hadryś <kugot999@gmail.com>
2019-03-28 23:22:20 +0100

    refactored the AlgoliaSearchContainer somewhat

--- src/pages/Marketplace/index.js
+++ src/pages/Marketplace/index.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react"
+import React, { Component, useState, useEffect } from "react"
 import { withBreakpoints } from "react-breakpoints"
 import { compose } from "recompose"
 import equal from "deep-equal"
@@ -8,6 +8,7 @@ import {
 	enableBodyScroll,
 	clearAllBodyScrollLocks
 } from "body-scroll-lock"
+import { withRouter } from "react-router-dom"
 
 import { withFirebase } from "../../components/Firebase"
 import AlgoliaResults from "../../components/Algolia/AlgoliaResults"
@@ -19,8 +20,9 @@ import Filters from "../../components/Filters"
 import Topbar from "../../components/Topbar"
 import { TextBlock } from "../../components/StyledComponents"
 
-import { MainGrid, Sidebar, StyledInstantSearch, GridContainer } from "./StyledComponents"
+import { MainGrid, Sidebar, GridContainer } from "./StyledComponents"
 import { CONST } from "../../constants"
+import { InstantSearch } from "react-instantsearch-dom"
 
 const DEFAULT_SORTING = sortingOptions[0].value
 const DEFAULT_HITS_PER_PAGE = 12
@@ -33,6 +35,100 @@ const DEFAULT_SEARCH_STATE = Object.freeze({
 })
 
 const createURL = (state) => `?search=${btoa(JSON.stringify(state))}`
+
+const AlgoliaSearchContainer = withRouter(
+	({
+		indexName,
+		onSearchStateChange,
+		urlToState,
+		children,
+		history,
+		location,
+		...rest
+	}) => {
+		const [searchState, setSearchState] = useState(DEFAULT_SEARCH_STATE)
+		const [shouldRefresh, setShouldRefresh] = useState(false)
+
+		useEffect(() => {
+			const state = createStateFromURL()
+			setSearchState(state)
+		}, [location])
+
+		useEffect(() => {
+			/* this is required in order for the component to not get stuck on a later
+		page without the ability to navigate back other than clearing all filters */
+
+			let newState = { ...searchState, page: 1 }
+			debugger
+			setSearchState(newState)
+		}, [])
+
+		const refresh = () => {
+			setShouldRefresh(true)
+			setShouldRefresh(false)
+		}
+
+		const shouldScroll = (oldState, newState) => {
+			/* get copies of current and prev states and compare all values except for page
+		to determine whether the component should scroll back to the top */
+			const _oldState = { ...oldState, page: null }
+			const _newState = { ...newState, page: null }
+			const areEqual = equal(_newState, _oldState)
+			if (!areEqual) {
+				window.scrollTo(0, 0)
+			}
+		}
+
+		const handleSearchStateChange = async (newSearchState) => {
+			// const _newSearchState = await cloneDeep(newSearchState)
+
+			// shouldScroll(cloneDeep(searchState), _newSearchState)
+
+			const formattedState = await onSearchStateChange(newSearchState)
+			const url = createURL(formattedState)
+			debugger
+			history.push(url)
+
+			// update the searchState (to increase apparent performance)
+			// setSearchState(newSearchState)
+		}
+
+		const createStateFromURL = () => {
+			try {
+				// get the encoded search parameter from URL
+				var searchParams = new URLSearchParams(location.search)
+				const search = searchParams.get("search")
+
+				// decode and parse the search paramter
+				const convertedSearch = atob(search)
+				const parsedSearch = JSON.parse(convertedSearch)
+
+				const formattedState = urlToState(parsedSearch)
+				debugger
+				return formattedState
+			} catch (e) {
+				console.log(e)
+				// if there was a problem while parsing, use default state instead
+				return DEFAULT_SEARCH_STATE
+			}
+		}
+
+		return (
+			<InstantSearch
+				appId={process.env.REACT_APP_APP_ID}
+				apiKey={process.env.REACT_APP_ALGOLIA_API_KEY}
+				indexName={CONST.DEV_ITEMS_MARKETPLACE_DEFAULT_ALGOLIA_INDEX}
+				searchState={searchState}
+				onSearchStateChange={handleSearchStateChange}
+				createURL={createURL}
+				refresh={shouldRefresh}
+				{...rest}
+			>
+				{children}
+			</InstantSearch>
+		)
+	}
+)
 
 class HomePage extends Component {
 	state = {
@@ -48,81 +144,33 @@ class HomePage extends Component {
 	setClearFiltersFlag = (to) => this.setState({ clearFilters: to })
 
 	componentDidMount() {
-		/* this is required in order for the component to not get stuck on a later
-		page without the ability to navigate back other than clearing all filters */
-		let searchState = { ...this.urlToState(), page: 1 }
-
 		this.targetElement = document.querySelector("#filters-container")
-
-		this.setState({ searchState, isLoading: false })
 	}
 
-	refreshAlgolia = () => {
-		this.setState({ refreshAlgolia: true }, () => {
-			this.setState({ refreshAlgolia: false })
-		})
-	}
-
-	urlToState = () => {
+	urlToState = (parsedSearch) => {
 		let searchState = cloneDeep(DEFAULT_SEARCH_STATE)
+		// format the searchState according to Algolia's spec
+		const { category, designers, size, price, sortBy, query, page } = parsedSearch
 
-		try {
-			// get the encoded search parameter from URL
-			var searchParams = new URLSearchParams(this.props.location.search)
-			const search = searchParams.get("search")
-
-			// decode and parse the search paramter
-			const convertedSearch = atob(search)
-			const parsedSearch = JSON.parse(convertedSearch)
-
-			// format the searchState according to Algolia's spec
-			const { category, designers, size, price, sortBy, query, page } = parsedSearch
-
-			searchState.refinementList.category = category || []
-			searchState.refinementList.designers = designers || []
-			searchState.refinementList.size = size || []
-			if (price) {
-				searchState.range = {}
-				searchState.range.price = price
-			} else {
-				delete searchState.range
-			}
-			searchState.sortBy = sortBy || DEFAULT_SORTING
-			searchState.query = query || ""
-			searchState.page = page || 1
-		} catch (e) {
-			console.log(e)
-			// if there was a problem while parsing, use default state instead
-			return DEFAULT_SEARCH_STATE
+		searchState.refinementList.category = category || []
+		searchState.refinementList.designers = designers || []
+		searchState.refinementList.size = size || []
+		if (price) {
+			searchState.range = {}
+			searchState.range.price = price
+		} else {
+			delete searchState.range
 		}
+		searchState.sortBy = sortBy || DEFAULT_SORTING
+		searchState.query = query || ""
+		searchState.page = page || 1
+
 		return searchState
 	}
 
-	componentDidUpdate(prevProps) {
-		if (prevProps.location !== this.props.location) {
-			const searchState = this.urlToState()
-			this.setState({ searchState })
-		}
-	}
-
-	shouldScroll = (oldState, newState) => {
-		/* get copies of current and prev states and compare all values except for page
-		to determine whether the component should scroll back to the top */
-		const _oldState = { ...oldState, page: null }
-		const _newState = { ...newState, page: null }
-		const areEqual = equal(_newState, _oldState)
-		if (!areEqual) {
-			document.getElementById("App-Element").scrollIntoView(true)
-		}
-	}
-
-	onSearchStateChange = async (searchState) => {
-		const _searchState = await cloneDeep(searchState)
-
-		this.shouldScroll(cloneDeep(this.state.searchState), _searchState)
-
+	onSearchStateChange = async (newSearchState) => {
 		// format the state to keep the url relatively short
-		const { refinementList, query, range, sortBy, page } = _searchState
+		const { refinementList, query, range, sortBy, page } = newSearchState
 		let formattedState = {}
 		if (refinementList !== undefined) {
 			if (refinementList.category !== undefined)
@@ -136,10 +184,7 @@ class HomePage extends Component {
 		if (sortBy !== undefined) formattedState.sortBy = sortBy
 		if (range && range.price !== undefined) formattedState.price = range.price
 
-		this.props.history.push(createURL(formattedState))
-
-		// update the searchState (to increase apparent performance)
-		this.setState({ searchState })
+		return formattedState
 	}
 
 	toggleFilters = () => {
@@ -156,45 +201,23 @@ class HomePage extends Component {
 		})
 	}
 
-	refresh = (e) => {
-		// Start the button animation
-		const target = e.currentTarget
-		target.classList.remove("spin")
-		target.classList.add("spin")
-
-		// Refresh Algolia cache
-		this.refreshAlgolia()
-
-		// Scroll to top
-		document.getElementById("App-Element").scrollIntoView(true)
-
-		// End the animation
-		setTimeout(() => target.classList.remove("spin"), 1500)
-	}
-
 	componentWillUnmount() {
 		clearAllBodyScrollLocks()
 	}
 
 	render() {
-		const { areFiltersOpen, searchState } = this.state
+		const { areFiltersOpen } = this.state
 		const { currentBreakpoint } = this.props
 
 		if (currentBreakpoint > 0) {
 			enableBodyScroll(this.targetElement)
 		}
 
-		return this.state.isLoading ? (
-			<LoadingSpinner />
-		) : (
-			<StyledInstantSearch
-				appId={process.env.REACT_APP_APP_ID}
-				apiKey={process.env.REACT_APP_ALGOLIA_API_KEY}
+		return (
+			<AlgoliaSearchContainer
 				indexName={CONST.DEV_ITEMS_MARKETPLACE_DEFAULT_ALGOLIA_INDEX}
-				searchState={searchState}
 				onSearchStateChange={this.onSearchStateChange}
-				createURL={createURL}
-				refresh={this.state.refreshAlgolia}
+				urlToState={this.urlToState}
 			>
 				{/* <TextBlock bold uppercase>
 					🔥 Promowane
@@ -227,7 +250,7 @@ class HomePage extends Component {
 					</MainGrid>
 				</GridContainer>
 				<ScrollToTop>↑</ScrollToTop>
-			</StyledInstantSearch>
+			</AlgoliaSearchContainer>
 		)
 	}
 }
