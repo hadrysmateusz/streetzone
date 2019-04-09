24ab2327911e13c45d018495b3e34fd5ea1d1241
Mateusz Hadryś <kugot999@gmail.com>
2019-04-09 18:52:13 +0200

    add some tests for formatItemData
    only testing the CREATE mode though

---
+++ src/utils/formatting/formatItemData.test.js
@@ -0,0 +1,93 @@
+import cloneDeep from "clone-deep"
+import { formatItemDataForDb, MODE, REQUIRED } from "./formatItemData"
+
+const requiredData = {
+	name: "Mock shoes",
+	designers: ["Balenciaga", "Nike"],
+	category: "Buty",
+	price: "550",
+	condition: "11.5",
+	attachments: ["attachment1", "attachment2"],
+	userId: "userid"
+}
+
+const optionalData = {
+	size: "buty-42",
+	description: "lorem ipsum dolor sit amet"
+}
+
+const fullData = {
+	...requiredData,
+	...optionalData
+}
+
+const expectedRequired = {
+	name: "Mock shoes",
+	designers: ["Balenciaga", "Nike"],
+	category: "Buty",
+	price: 550,
+	condition: 11.5,
+	attachments: ["attachment1", "attachment2"],
+	userId: "userid",
+	itemId: expect.any(String),
+	isArchived: false,
+	isVerified: false,
+	createdAt: expect.any(Number),
+	refreshedAt: expect.any(Number),
+	promotedAt: expect.any(Number),
+	modifiedAt: expect.any(Number)
+}
+
+const expectedFull = {
+	...expectedRequired,
+	size: "buty-42",
+	description: "lorem ipsum dolor sit amet"
+}
+
+test("creates item from correct data", () => {
+	const formatted = formatItemDataForDb(fullData, MODE.CREATE)
+	expect(formatted).toMatchObject(expectedFull)
+})
+
+test("handles optional data", () => {
+	const formatted = formatItemDataForDb(requiredData, MODE.CREATE)
+	expect(formatted).toMatchObject(expectedRequired)
+})
+
+test("throws if missing required data", () => {
+	const data = cloneDeep(fullData)
+
+	for (const field of REQUIRED) {
+		expect(() => {
+			formatItemDataForDb({ ...data, [field]: undefined }, MODE.CREATE)
+		}).toThrow("missing required data")
+	}
+})
+
+test("throws if invalid designers value", () => {
+	const data = cloneDeep(fullData)
+
+	data.designers = []
+	expect(() => {
+		formatItemDataForDb(data, MODE.CREATE)
+	}).toThrow("a property expected a non-empty array")
+
+	data.designers = "some designer"
+	expect(() => {
+		formatItemDataForDb(data, MODE.CREATE)
+	}).toThrow("a property expected a non-empty array")
+})
+
+test("throws if invalid attachments value", () => {
+	const data = cloneDeep(fullData)
+
+	data.attachments = []
+	expect(() => {
+		formatItemDataForDb(data, MODE.CREATE)
+	}).toThrow("a property expected a non-empty array")
+
+	data.attachments = "some attachment"
+	expect(() => {
+		formatItemDataForDb(data, MODE.CREATE)
+	}).toThrow("a property expected a non-empty array")
+})
