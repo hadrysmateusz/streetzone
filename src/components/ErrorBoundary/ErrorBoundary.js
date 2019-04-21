d79ebf7784c5674adccc67b593519556fc240d2c
Mateusz Hadrys <kugot999@gmail.com>
2019-04-21 17:17:07 +0200

    changes to ErrorBoundary
    should only report errors in production

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,29 +1,27 @@
 import React from "react"
 import * as Sentry from "@sentry/browser"
-import Button from "../Button"
 
 class ErrorBoundary extends React.Component {
 	state = { error: null, errorInfo: null, eventId: null }
 
 	componentDidCatch(error, errorInfo) {
 		this.setState({ error })
-		Sentry.withScope((scope) => {
-			scope.setExtras(errorInfo)
-			const eventId = Sentry.captureException(error)
-			this.setState({ eventId })
-		})
+
+		// send errors to Sentry while in production
+		if (process.env.NODE_ENV === "production") {
+			Sentry.withScope((scope) => {
+				scope.setExtras(errorInfo)
+				const eventId = Sentry.captureException(error)
+				this.setState({ eventId })
+			})
+		}
 	}
 
 	render() {
 		const { error, errorInfo } = this.state
 		const { ErrorComponent } = this.props
 		return error ? (
-			<>
-				<ErrorComponent error={error} errorInfo={errorInfo} />
-				<Button onClick={() => Sentry.showReportDialog({ eventId: this.state.eventId })}>
-					Report feedback
-				</Button>
-			</>
+			<ErrorComponent error={error} errorInfo={errorInfo} />
 		) : (
 			this.props.children
 		)
