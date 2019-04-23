82dd9e79820d57f4e4d584743ffaf36f52bf6ab7
Mateusz Hadrys <kugot999@gmail.com>
2019-04-23 19:31:54 +0200

    add user info to sentry

--- src/components/ErrorBoundary/ErrorBoundary.js
+++ src/components/ErrorBoundary/ErrorBoundary.js
@@ -1,16 +1,26 @@
 import React from "react"
 import * as Sentry from "@sentry/browser"
+import { withAuthentication } from "../UserSession"
 
 class ErrorBoundary extends React.Component {
 	state = { error: null, errorInfo: null, eventId: null }
 
 	componentDidCatch(error, errorInfo) {
 		this.setState({ error })
+		const { authUser } = this.props
 
-		// send errors to Sentry while in production
+		// prevent double errors because of dev rethrow
 		if (process.env.NODE_ENV === "production") {
+			// report to sentry
 			Sentry.withScope((scope) => {
 				scope.setExtras(errorInfo)
+				if (authUser) {
+					scope.setUser({
+						id: authUser.uid,
+						email: authUser.email,
+						username: authUser.name
+					})
+				}
 				const eventId = Sentry.captureException(error)
 				this.setState({ eventId })
 			})
@@ -28,4 +38,4 @@ class ErrorBoundary extends React.Component {
 	}
 }
 
-export default ErrorBoundary
+export default withAuthentication(ErrorBoundary)
