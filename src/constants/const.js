7a31199c9f913f545e5273a123ab5ecb9e401d06
Mateusz Hadrys <kugot999@gmail.com>
2019-04-23 15:33:03 +0200

    update env variables setup

--- src/constants/const.js
+++ src/constants/const.js
@@ -13,12 +13,10 @@ export const L_THUMB_POSTFIX = "_L_THUMB"
 
 // ===============================================================================
 // !!! REMEMBER TO ALSO CHANGE IN THE CLOUD FUNCTIONS DIRECTORY !!!
-const isProd = process.env.NODE_ENV === "production"
-export const ITEMS_MARKETPLACE_DEFAULT_ALGOLIA_INDEX = isProd ? "prod_items" : "dev_items"
-export const ITEMS_MARKETPLACE_PRICE_ASC_ALGOLIA_INDEX = isProd
-	? "prod_items_price_asc"
-	: "dev_items_price_asc"
-export const ITEMS_CUSTOM_ALGOLIA_INDEX = isProd ? "prod_custom" : "dev_custom"
-export const BLOG_ALGOLIA_INDEX = isProd ? "prod_posts" : "dev_posts"
-export const DESIGNERS_ALGOLIA_INDEX = isProd ? "prod_designers" : "dev_designers"
+const env = process.env.REACT_APP_ENV
+export const ITEMS_MARKETPLACE_DEFAULT_ALGOLIA_INDEX = `${env}_items`
+export const ITEMS_MARKETPLACE_PRICE_ASC_ALGOLIA_INDEX = `${env}_items_price_asc`
+export const ITEMS_CUSTOM_ALGOLIA_INDEX = `${env}_custom`
+export const BLOG_ALGOLIA_INDEX = `${env}_posts`
+export const DESIGNERS_ALGOLIA_INDEX = `${env}_designers`
 // ===============================================================================
