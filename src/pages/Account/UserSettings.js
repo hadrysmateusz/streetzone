3d701160bd3a6f8774629366846def05c101eb7e
Mateusz Hadrys <kugot999@gmail.com>
2019-04-24 13:51:36 +0200

    add rudimentary delete account button

--- src/pages/Account/UserSettings.js
+++ src/pages/Account/UserSettings.js
@@ -5,9 +5,68 @@ import SignOutButton from "../../components/SignOut"
 import ProfileEdit from "../../components/ProfileEdit"
 import { PageContainer } from "../../components/Containers"
 import { Separator } from "../../components/Basics"
+import { Button } from "../../components/Button"
+
+import { useFirebase } from "../../hooks"
 
 import { UserSettingsContainer, Section } from "./StyledComponents"
 import UserPreferences from "./UserPreferences"
+
+const DeleteAccountButton = () => {
+	const firebase = useFirebase()
+
+	const onDelete = async () => {
+		const confirmation = window.confirm(
+			"Czy napewno chcesz usunąć swoje konto? Tej akcji nie można cofnąć."
+		)
+
+		if (!confirmation) return
+
+		/* TODO: email+password doesn't work yet. this needs a lot more work to communicate to the user what they need to do and the ui needs to reflect that */
+		try {
+			await firebase.auth.currentUser.delete()
+		} catch (error) {
+			if (error.code === "auth/requires-recent-login") {
+				const user = firebase.auth.currentUser
+
+				const signInMethods = await firebase.auth.fetchSignInMethodsForEmail(user.email)
+
+				// try google
+				if (signInMethods.includes(firebase.signInMethods.google)) {
+					await user.reauthenticateWithPopup(firebase.googleProvider)
+					await firebase.auth.currentUser.delete()
+
+					return null
+				}
+
+				// try facebook
+				if (signInMethods.includes(firebase.signInMethods.facebook)) {
+					await user.reauthenticateWithPopup(firebase.facebookProvider)
+					await firebase.auth.currentUser.delete()
+
+					return null
+				}
+
+				// try email and password
+				if (signInMethods.includes(firebase.signInMethods.email))
+					return async (password) => {
+						const credential = firebase.emailAuthProvider.credential(user.email, password)
+						await user.reauthenticateAndRetrieveDataWithCredential(credential)
+						await firebase.auth.currentUser.delete()
+					}
+			} else {
+				// if there was a different error, rethrow it
+				throw error
+			}
+		}
+	}
+
+	return (
+		<Button onClick={onDelete} big fullWidth>
+			Usuń konto
+		</Button>
+	)
+}
 
 const UserSettings = () => (
 	<PageContainer>
@@ -27,6 +86,10 @@ const UserSettings = () => (
 			</Section>
 
 			<Separator />
+
+			<Section>
+				<DeleteAccountButton />
+			</Section>
 
 			<Section>
 				<SignOutButton />
