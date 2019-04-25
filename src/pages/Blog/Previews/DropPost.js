30e27cb0c162dcbddd185e7995be57a19b3a3527
Mateusz Hadrys <kugot999@gmail.com>
2019-04-25 11:03:02 +0200

    improve code style

--- src/pages/Blog/Previews/DropPost.js
+++ src/pages/Blog/Previews/DropPost.js
@@ -19,7 +19,7 @@ const DropPost = ({ id, mainImageURL, title, dropsAt }) => {
 						{title}
 					</TextBlock>
 					<TextBlock serif color="black75">
-						{moment(Date.now()).to(moment(dropsAt))}
+						{moment().to(moment(dropsAt))}
 					</TextBlock>
 					<ButtonContainer>
 						<Button>Czytaj więcej</Button>
