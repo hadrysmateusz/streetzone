97fa0584748355812be21ed80e1d1599d8ba25ed
Mateusz Hadryś <kugot999@gmail.com>
2019-05-18 15:19:03 +0200

    improve scroll to top positioning

--- src/components/ScrollToTop/index.js
+++ src/components/ScrollToTop/index.js
@@ -22,12 +22,11 @@ const ScrollToTop = styled.button.attrs((p) => ({
 	font-size: var(--font-size);
 	bottom: var(--offset);
 	right: var(--offset);
-	margin-bottom: var(--offset);
 	/* 
 	make the elements above ignore the height and offset of the button 
 	the additional 20px is to make the size of the last item visible
 	*/
-	margin-top: calc((var(--size) + var(--offset) + 20px) * -1);
+	margin-top: calc((var(--size) + var(--offset)) * -1);
 	width: var(--size);
 	height: var(--size);
 	@media (min-width: ${(p) => p.theme.breakpoints[1]}px) {
