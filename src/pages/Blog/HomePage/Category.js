715c4faaf5710c09c7f3115d11e43759adf437ea
Mateusz Hadryś <kugot999@gmail.com>
2019-05-25 12:12:03 +0200

    functional blog category page

--- src/pages/Blog/HomePage/Category.js
+++ src/pages/Blog/HomePage/Category.js
@@ -1,7 +1,53 @@
 import React from "react"
+import styled from "styled-components/macro"
+import { withBreakpoints } from "react-breakpoints"
+import { compose } from "recompose"
+import { withRouter } from "react-router"
 
-const Category = () => {
-	return <div>CATEGORY</div>
+import { CONST } from "../../../constants"
+
+import { PageContainer } from "../../../components/Containers"
+import { StatelessSearchWrapper } from "../../../components/InstantSearchWrapper"
+
+import Sidebar from "./Sidebar"
+import InfinitePosts from "../InfinitePostsList"
+
+const Layout = styled.div`
+	@media (min-width: ${(p) => p.theme.breakpoints[2]}px) {
+		display: grid;
+		grid-template-columns: 1fr minmax(220px, 25%);
+		gap: var(--spacing3);
+	}
+`
+
+const BlogCategoryPage = ({ currentBreakpoint, match }) => {
+	const isMobile = currentBreakpoint <= 1
+
+	const { category } = match.params
+
+	return (
+		<>
+			<StatelessSearchWrapper
+				indexName={CONST.BLOG_POST_ALGOLIA_INDEX}
+				refinements={{ category }}
+				hitsPerPage={6}
+			>
+				<PageContainer>
+					<Layout>
+						{/* Main Content */}
+						<main>
+							<InfinitePosts />
+						</main>
+						{/* Sidebar */}
+						{!isMobile && <Sidebar />}
+					</Layout>
+				</PageContainer>
+			</StatelessSearchWrapper>
+		</>
+	)
 }
 
-export default Category
+export default compose(
+	withBreakpoints,
+	withRouter
+)(BlogCategoryPage)
