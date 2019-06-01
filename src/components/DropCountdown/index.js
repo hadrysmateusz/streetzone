2dd9057cb666bee0ba9bac60c66b1b3394822b59
Mateusz Hadryś <kugot999@gmail.com>
2019-06-01 22:44:53 +0200

    improve/fix drop countdown

--- src/components/DropCountdown/index.js
+++ src/components/DropCountdown/index.js
@@ -34,45 +34,60 @@ const CountdownContainer = styled.div`
 	}
 `
 
+const formatValue = (dropsAt) => {
+	const then = moment(dropsAt, dateFormat)
+	const now = moment()
+
+	const duration = moment.duration(then.diff(now))
+
+	const totalHours = Math.floor(duration.asHours())
+	const totalDays = Math.floor(duration.asDays())
+	const minutes = duration.minutes()
+
+	const isDropInFuture = then.isAfter(now) // this isn't reliable for dates without specified time
+	const isHourKnown = dropsAt && dropsAt.length > 11
+	const isSoon = totalDays && totalDays <= 2
+	const isToday = now.isSame(then, "day")
+
+	// isDropInFuture isn't reliable for dates without specified time so it requires a special case
+	if (!isDropInFuture && isToday) {
+		return "Dzisiaj"
+	}
+
+	// all further cases require the date to be in the future
+	if (!isDropInFuture) {
+		return null
+	}
+
+	// 21 hours is when moment starts to show hours instead of days
+	// if the hour isn't known, prevent from showing misleading information
+	if (!isHourKnown && totalHours <= 21) {
+		return "Jutro"
+	}
+
+	// if the drop is very soon and the exact time is known, display countdown
+	if (isSoon) {
+		return `${totalHours}h ${minutes}min`
+	}
+
+	// by default return date formatted by moment.js
+	return moment().to(then, true)
+}
+
 const DropCountdown = ({ dropsAt, id }) => {
 	const [value, setValue] = useState(null)
 
 	useEffect(() => {
-		const getValues = () => {
-			const then = moment(dropsAt, dateFormat)
-			const now = moment()
-
-			const isDropInFuture = then.isAfter(now)
-
-			if (isDropInFuture) {
-				const duration = moment.duration(then.diff(now))
-
-				const totalHours = Math.floor(duration.asHours())
-				const totalDays = Math.floor(duration.asDays())
-				const minutes = duration.minutes()
-
-				const isHourKnown = dropsAt && dropsAt.length > 11
-				const isSoon = totalDays && totalDays <= 3
-				const isToday = totalHours <= 21 // based on moment.js "time to" breakpoints
-
-				// format value based on a few parameters
-				if (isHourKnown && isSoon) {
-					setValue(`${totalHours}h ${minutes}min`)
-				} else if (!isHourKnown && isToday) {
-					setValue("Dzisiaj") // TODO: this might be inaccessible because of conflict with isDropInFuture
-				} else {
-					setValue(moment().to(then, true))
-				}
-			} else {
-				setValue(null)
-			}
+		const getValue = () => {
+			const value = formatValue(dropsAt)
+			setValue(value)
 		}
 
 		// get first values instantly
-		getValues()
+		getValue()
 
-		// update every 20 seconds
-		const id = setInterval(getValues, 20000)
+		// update every minute
+		const id = setInterval(getValue, 60000)
 
 		return () => clearInterval(id)
 	}, [dropsAt])
