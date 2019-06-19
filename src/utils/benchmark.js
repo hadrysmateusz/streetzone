9b3781eb0ef73387b140a2e019f57b7b14374e82
Mateusz Hadryś <kugot999@gmail.com>
2019-06-19 21:50:36 +0200

    remove deprecated utilities

--- src/utils/benchmark.js
+++
@@ -1,6 +0,0 @@
-export default async function benchmark(fn, ...params) {
-	let start = Date.now()
-	await fn(...params)
-	let end = Date.now()
-	console.log(`Execution took: ${end - start}`)
-}
--- src/utils/errorLog.js
+++
@@ -1,3 +0,0 @@
-export default (dev, prod) => {
-	process.env.NODE_ENV === "development" ? console.log(dev) : console.log(prod)
-}
