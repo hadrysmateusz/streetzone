f3774d2cb9b30b2c2a299319be3556860b0ff0ad
Mateusz Hadryś <kugot999@gmail.com>
2019-06-24 14:34:56 +0200

    new delay render hook

--- src/hooks/useDelayRender.js
+++ src/hooks/useDelayRender.js
@@ -6,7 +6,30 @@ export default (time) => {
 	useEffect(() => {
 		const timeoutId = setTimeout(() => setIsDone(true), time)
 		return () => clearTimeout(timeoutId)
-	}, [])
+	}, [time])
 
 	return isDone
+}
+
+export const useNewDelayRender = (render, delay = 0) => {
+	const [isDone, setIsDone] = useState(false)
+
+	if (!delay) {
+		console.log(
+			"Delay was not set in a delayRender hook (the component will render without delay)"
+		)
+	}
+
+	useEffect(() => {
+		// render immediately if delay is set to 0
+		if (delay === 0) {
+			setIsDone(true)
+			return
+		}
+
+		const timeoutId = setTimeout(() => setIsDone(true), delay)
+		return () => clearTimeout(timeoutId)
+	}, [delay])
+
+	return isDone ? render : null
 }
