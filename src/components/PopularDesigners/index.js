13d4e8cadf056fba2c507a9b62ded65ec24289fe
Mateusz Hadryś <kugot999@gmail.com>
2019-06-30 17:00:19 +0200

    create working marketplace promoted home component

---
+++ src/components/PopularDesigners/index.js
@@ -0,0 +1,7 @@
+// import React from "react"
+
+// const PopularDesigners = () => {
+// 	return <OuterContainer></OuterContainer>
+// }
+
+// export default PopularDesigners
--- src/pages/Home/MarketplacePromoted.js
+++ src/pages/Home/MarketplacePromoted.js
@@ -1,27 +1,90 @@
 import React from "react"
 import styled from "styled-components/macro"
 
+import { LinkButton, ButtonContainer } from "../../components/Button"
+import { FluidImage } from "../../components/Image"
+import { StatelessSearchWrapper } from "../../components/InstantSearchWrapper"
+import PageHeading from "../../components/PageHeading"
+
 import { CONST } from "../../constants"
+import { useImage } from "../../hooks"
+import { itemDataHelpers, route } from "../../utils"
+import { ellipsis, nLinesHigh } from "../../style-utils"
 
-const OuterContainer = styled.div``
+const { formatDesigners } = itemDataHelpers
 
-const Heading = styled.div``
-const StatelessSearchWrapper = styled.div``
-const InnerContainer = styled.div``
-const PromotedItemContainer = styled.div``
+const OuterContainer = styled.div`
+	margin-top: var(--spacing6);
+`
+
+const InnerContainer = styled.div`
+	display: grid;
+	gap: var(--spacing3);
+	@media (min-width: ${(p) => p.theme.breakpoints[3]}px) {
+		grid-template-columns: 1fr 1fr;
+		gap: var(--spacing4);
+	}
+`
+const PromotedItemContainer = styled.div`
+	display: grid;
+	grid-template-columns: 1fr minmax(180px, 0.5fr);
+	@media (min-width: ${(p) => p.theme.breakpoints[3]}px) {
+		grid-template-columns: 1fr minmax(180px, 0.8fr);
+	}
+	gap: var(--spacing3);
+`
+const InfoContainer = styled.div`
+	padding: var(--spacing3) 0;
+`
+const Designers = styled.div`
+	font-size: var(--font-size--s);
+	color: var(--gray0);
+	text-transform: uppercase;
+	margin-bottom: var(--spacing3);
+
+	${ellipsis}
+`
+const Name = styled.div`
+	font-size: var(--font-size--l);
+	font-weight: var(--semi-bold);
+	${nLinesHigh(2, { lineHeight: 1.3 })}
+	margin-bottom: var(--spacing3);
+	text-overflow: ellipsis;
+`
+
+const PromotedItem = ({ attachments, mainImageIndex, name, designers, price, id }) => {
+	const { imageURL } = useImage(attachments[mainImageIndex], "M")
+	const formattedDesigners = formatDesigners(designers)
+
+	console.log(imageURL)
+
+	return (
+		<PromotedItemContainer>
+			<InfoContainer>
+				<Designers>{formattedDesigners}</Designers>
+				<Name title={name}>{name}</Name>
+				<ButtonContainer noMargin>
+					<LinkButton to={route("ITEM_DETAILS", { id })}>Kup za {price}zł</LinkButton>
+				</ButtonContainer>
+			</InfoContainer>
+			<FluidImage url={imageURL} />
+		</PromotedItemContainer>
+	)
+}
 
 const MarketplacePromoted = () => {
 	return (
 		<OuterContainer>
-			<Heading>Promowane na tablicy</Heading>
+			<PageHeading emoji={"🔥"}>Promowane na tablicy</PageHeading>
 			<StatelessSearchWrapper
 				indexName={CONST.ITEMS_MARKETPLACE_DEFAULT_ALGOLIA_INDEX}
 				refinements={{ promotedUntil: { min: Date.now() } }}
-				limit={4}
 			>
-				{(results) => (
+				{(hits) => (
 					<InnerContainer>
-						{(results) => results.map((result) => <PromotedItemContainer {...result} />)}
+						{hits.map((result) => (
+							<PromotedItem {...result} />
+						))}
 					</InnerContainer>
 				)}
 			</StatelessSearchWrapper>
