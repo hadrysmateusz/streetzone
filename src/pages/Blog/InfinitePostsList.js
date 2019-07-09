2bbaed224f6b0f892a399f0c55bbed3593f4f4e7
Mateusz Hadryś <kugot999@gmail.com>
2019-07-09 22:08:02 +0200

    change gap between posts

--- src/pages/Blog/InfinitePostsList.js
+++ src/pages/Blog/InfinitePostsList.js
@@ -8,7 +8,7 @@ const PostsContainer = styled.div`
 	display: grid;
 	grid-template-columns: 100%;
 
-	gap: 10px;
+	gap: var(--spacing3);
 	@media (max-width: ${(p) => p.theme.breakpoints[2] - 1}px) {
 		margin: 0 calc(var(--spacing3) * -1);
 	}
