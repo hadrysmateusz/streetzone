5a028e36cc9e353c420148c536ea3f2065ab2a24
Mateusz Hadryś <kugot999@gmail.com>
2019-07-10 16:23:14 +0200

    remove old code

--- src/components/InstantSearchWrapper/index.js
+++ src/components/InstantSearchWrapper/index.js
@@ -1,13 +1,4 @@
-import { UncontrolledInstantSearchWrapper, InstantSearchWrapper } from "./old"
 import SearchWrapper from "./stateful"
 import StatelessSearchWrapper, { SearchWrapperContext } from "./stateless"
 
-export default InstantSearchWrapper
-
-export {
-	SearchWrapper,
-	StatelessSearchWrapper,
-	InstantSearchWrapper,
-	UncontrolledInstantSearchWrapper,
-	SearchWrapperContext
-}
+export { SearchWrapper, StatelessSearchWrapper, SearchWrapperContext }
