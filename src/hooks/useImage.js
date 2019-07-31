4e20e8f5a028db498ec7d0c69777d6d5969d7f69
Mateusz Hadryś <kugot999@gmail.com>
2019-07-31 19:15:07 +0200

    make useImage construct url manually

--- src/hooks/useImage.js
+++ src/hooks/useImage.js
@@ -1,7 +1,22 @@
 import { useState, useEffect } from "react"
 import useFirebase from "./useFirebase"
 
+import { S_THUMB_POSTFIX, M_THUMB_POSTFIX, L_THUMB_POSTFIX } from "../constants/const"
+
 const ERR_NO_IMAGE = "NO_IMAGE"
+
+const getSuffixForSize = (size) => {
+	switch (size) {
+		case "S":
+			return S_THUMB_POSTFIX
+		case "M":
+			return M_THUMB_POSTFIX
+		case "L":
+			return L_THUMB_POSTFIX
+		default:
+			return ""
+	}
+}
 
 export default function useImage(storageRef, size, defer) {
 	const firebase = useFirebase()
@@ -11,7 +26,11 @@ export default function useImage(storageRef, size, defer) {
 	useEffect(() => {
 		const fetchUrl = async () => {
 			try {
-				const imageURL = await firebase.getImageURL(storageRef, size)
+				let suffix = getSuffixForSize(size)
+				let bucket = process.env.REACT_APP_STORAGE_BUCKET
+				let imagePath = encodeURIComponent(storageRef)
+				const imageURL = `https://firebasestorage.googleapis.com/v0/b/${bucket}/o/${imagePath}${suffix}?alt=media`
+
 				setImageURL(imageURL)
 			} catch (error) {
 				setError(ERR_NO_IMAGE)
