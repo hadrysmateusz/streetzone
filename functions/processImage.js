c4f0ea15ff7e1518fd6046e6ce0296eb3be0b5dd
Mateusz Hadryś <kugot999@gmail.com>
2019-07-31 15:00:54 +0200

    more changes to image processing

--- functions/processImage.js
+++ functions/processImage.js
@@ -18,31 +18,62 @@ const {
 	STORAGE_BUCKET_PROFILE_PICTURES
 } = require("./const")
 
+/**
+ * Converts file to JPEG format
+ * @param {string} sourceFilePath temporary local path to the downloaded source path
+ * @param {string} tempWorkingPath local path where the converted path will temporarily be stored
+ * @param {string} filePath path to file in cloud storage
+ */
+const convertFileToJPEG = async (sourceFilePath, filePath) => {
+	const fileName = path.basename(filePath, path.extname(filePath))
+	const fileDir = path.dirname(filePath)
+	const thumbnailPath = path.normalize(
+		path.format({ dir: fileDir, name: fileName, ext: JPEG_EXTENSION })
+	)
+	const localTempPath = path.join(os.tmpdir(), thumbnailPath)
+
+	// Convert the image to JPEG using ImageMagick.
+	await spawn("convert", [sourceFilePath, localTempPath])
+
+	// Remove the original file
+	await bucket.file(filePath).delete()
+
+	// Upload the JPEG image (To the location of the original file)
+	await bucket.upload(localTempPath, { destination: filePath })
+
+	fs.unlinkSync(localTempPath)
+}
+
 const signedURLConfig = {
 	action: "read",
 	expires: "03-01-2500"
 }
 
-const generateThumbnail = ({ size, mode }, pathFrom, pathTo) => {
+const generateThumbnail = async (pathFrom, pathTo, options) => {
+	const { size, mode } = options
+
 	console.log(pathFrom, pathTo)
 
-	if (mode === "cover") {
-		return spawn(
-			"convert",
-			[pathFrom, "-thumbnail", size + "^", "-gravity", "center", "-extent", size, pathTo],
-			{
+	switch (mode) {
+		case "contain":
+			return spawn("convert", [pathFrom, "-thumbnail", size, pathTo], {
 				capture: ["stdout", "stderr"]
-			}
-		)
-	} else if (mode === "contain") {
-		return spawn("convert", [pathFrom, "-thumbnail", size, pathTo], {
-			capture: ["stdout", "stderr"]
-		})
+			})
+		case "cover":
+			return spawn(
+				"convert",
+				[pathFrom, `-thumbnail ${size}^`, "-gravity center", `-extent ${size}`, pathTo],
+				{
+					capture: ["stdout", "stderr"]
+				}
+			)
+		default:
+			throw Error(`invalid processing mode (${mode})`)
 	}
 }
 
 const uploadThumbnail = async (file, path) => {
-	await bucket.upload(file, {
+	return bucket.upload(file, {
 		destination: path,
 		contentType: "image/jpeg",
 		public: true,
@@ -51,108 +82,92 @@ const uploadThumbnail = async (file, path) => {
 			cacheControl: "public, max-age=31557600"
 		}
 	})
-	return bucket.file(path).getSignedUrl(signedURLConfig)
 }
 
-const downloadFile = async (filePath) => {
-	const tempLocalFile = path.join(os.tmpdir(), filePath)
-	const tempLocalDir = path.dirname(tempLocalFile)
-
-	// Create the temp directory where the storage file will be downloaded
-	await mkdirp(tempLocalDir)
-
-	// Download file from bucket
-	await bucket.file(filePath).download({ destination: tempLocalFile })
-
-	return tempLocalFile
-}
-
-const generateThumbnailPath = (sourceFilePath, postfix = "") => {
-	const baseFileName = path.basename(sourceFilePath, path.extname(sourceFilePath))
-	const fileDir = path.dirname(sourceFilePath)
-
+const createThumbnail = async (filePath, sourceFilePath, postfix, thumbnailOptions) => {
+	const fileName = path.basename(filePath, path.extname(filePath))
+	const fileDir = path.dirname(filePath)
 	const thumbnailPath = path.normalize(
-		path.format({ dir: fileDir, name: baseFileName + postfix })
+		path.format({ dir: fileDir, name: fileName + postfix })
 	)
+	const localTempPath = path.join(os.tmpdir(), thumbnailPath)
+
+	// generate thumbnail
+	try {
+		await generateThumbnail(sourceFilePath, localTempPath, thumbnailOptions)
+	} catch (error) {
+		console.error("[spawn] stderr: ", error.stderr)
+		throw error
+	}
+	// upload thumbnail
+	await uploadThumbnail(localTempPath, thumbnailPath)
+	// remove temporary file to free up memory
+	fs.unlinkSync(localTempPath)
 
 	return thumbnailPath
 }
 
-const convertFileToJPEG = async (sourceFile, sourceFilePath) => {
-	const sourceFileName = path.basename(sourceFilePath, path.extname(sourceFilePath))
-	const sourceFileDir = path.dirname(sourceFilePath)
-	const tempFilePath = path.normalize(
-		path.format({ dir: sourceFileDir, name: sourceFileName, ext: JPEG_EXTENSION })
-	)
-	let tempFile = path.join(os.tmpdir(), tempFilePath)
-
-	// Convert the image to JPEG using ImageMagick.
-	await spawn("convert", [sourceFile, tempFile])
-
-	// Remove the original file
-	await bucket.file(sourceFilePath).delete()
-
-	// Upload the JPEG image (To the location of the original file)
-	await bucket.upload(tempFile, { destination: sourceFilePath })
-}
-
+/**
+ * generates thumbnails for a given file
+ * returns array of cloud storage paths for all images (thumbnails and the original image)
+ */
 const generateThumbnails = async (object, sizes) => {
 	const filePath = object.name
 	const contentType = object.contentType
 
 	// Exit if the file is not an image
 	if (!object.contentType.startsWith("image/")) {
-		console.log(`This file is not an image (${object.contentType}`)
+		console.log(`Exiting because file (${filePath}) is not an image (${contentType})`)
 		return null
 	}
 
 	// Exit if the file is already a thumbnail
 	if (filePath.endsWith("THUMB")) {
-		console.log("File is already a thumbnail")
+		console.log(`Exiting because file (${filePath}) is already a thumbnail`)
 		return false
 	}
 
-	// download file from storage bucket to local (on server) storage
-	const tempLocalFile = await downloadFile(filePath)
+	// Generate temporary paths
+	const sourceFilePath = path.join(os.tmpdir(), filePath)
+
+	// Make sure temporary directories exist
+	await mkdirp(path.dirname(sourceFilePath))
+
+	// Download file from bucket
+	await bucket.file(filePath).download({ destination: sourceFilePath })
 
 	// if file is in other format, convert it to jpeg
 	if (!contentType.startsWith("image/jpeg")) {
-		convertFileToJPEG(tempLocalFile, filePath)
+		convertFileToJPEG(sourceFilePath, filePath)
 	}
 
-	const signedUrls = []
-
-	signedUrls.push(bucket.file(filePath).getSignedUrl(signedURLConfig))
-
-	console.log("sizes: ", sizes)
+	// create array that will contain promises resolving to cloud storage paths of all images
+	const pathPromises = [filePath]
 
 	// Generate thumbnails using ImageMagick
-	if (sizes[0]) {
-		const thumbnailPath = generateThumbnailPath(filePath, S_THUMB_POSTFIX)
-		const thumbnailFile = path.join(os.tmpdir(), thumbnailPath)
-		await generateThumbnail(sizes[0], tempLocalFile, thumbnailFile)
-		const signedUrl = await uploadThumbnail(thumbnailFile, thumbnailPath)
-		signedUrls.push(signedUrl)
-		fs.unlinkSync(thumbnailFile)
+	if (sizes[2]) {
+		pathPromises.push(
+			createThumbnail(filePath, sourceFilePath, L_THUMB_POSTFIX, sizes[2])
+		)
 	}
 	if (sizes[1]) {
-		const thumbnailPath = generateThumbnailPath(filePath, M_THUMB_POSTFIX)
-		const thumbnailFile = path.join(os.tmpdir(), thumbnailPath)
-		await generateThumbnail(sizes[1], tempLocalFile, thumbnailFile)
-		const signedUrl = await uploadThumbnail(thumbnailFile, thumbnailPath)
-		signedUrls.push(signedUrl)
-		fs.unlinkSync(thumbnailFile)
+		pathPromises.push(
+			createThumbnail(filePath, sourceFilePath, M_THUMB_POSTFIX, sizes[1])
+		)
 	}
-	if (sizes[2]) {
-		const thumbnailPath = generateThumbnailPath(filePath, L_THUMB_POSTFIX)
-		const thumbnailFile = path.join(os.tmpdir(), thumbnailPath)
-		await generateThumbnail(sizes[2], tempLocalFile, thumbnailFile)
-		const signedUrl = await uploadThumbnail(thumbnailFile, thumbnailPath)
-		signedUrls.push(signedUrl)
-		fs.unlinkSync(thumbnailFile)
+	if (sizes[0]) {
+		pathPromises.push(
+			createThumbnail(filePath, sourceFilePath, S_THUMB_POSTFIX, sizes[0])
+		)
 	}
 
-	return signedUrls
+	// wait for all promises to resolve
+	const paths = await Promise.all(pathPromises)
+
+	// remove the source file (only after all promises resolve)
+	fs.unlinkSync(sourceFilePath)
+
+	return paths
 }
 
 const writeUrlsToDb = async (userId, urls) => {
@@ -207,13 +222,16 @@ const processImage = async (file) => {
 				{ size: "760x500", mode: "contain" }
 			])
 		case STORAGE_BUCKET_PROFILE_PICTURES:
-			const urls = await generateThumbnails(file, [
+			const filePaths = await generateThumbnails(file, [
 				{ size: "60x60", mode: "cover" },
 				{ size: "130x130", mode: "cover" },
 				{ size: "230x230", mode: "cover" }
 			])
-			if (urls) {
+			if (filePaths) {
 				const userId = file.name.split("/")[1]
+				const urls = await Promise.all(
+					filePaths.map((path) => bucket.file(path).getSignedUrl(signedURLConfig))
+				)
 				await writeUrlsToDb(userId, urls)
 			}
 			return
