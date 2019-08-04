9e4903df9c34b74119fa685b8fcc7c6f985912ee
Mateusz Hadryś <kugot999@gmail.com>
2019-08-04 23:11:24 +0200

    make icon buttons square

--- src/components/Button/index.js
+++ src/components/Button/index.js
@@ -190,8 +190,11 @@ const IconButtonUnstyled = ({ icon, ...rest }) => (
 	</Button>
 )
 
+// TODO: move from boolean big prop to a size prop
 const IconButton = styled(IconButtonUnstyled)`
-	padding: 0 var(--spacing3);
+	padding: 0;
+	height: ${(p) => (p.big ? "48px" : "40px")};
+	width: ${(p) => (p.big ? "48px" : "40px")};
 `
 
 const LoaderButtonUnstyled = ({ isLoading, text, loadingText = text, ...rest }) => (
