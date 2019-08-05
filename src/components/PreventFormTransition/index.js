36b6ed53f7615456cca01c25290f66888cc80ab8
Mateusz Hadryś <kugot999@gmail.com>
2019-08-05 13:27:17 +0200

    create component for form transition blocking

---
+++ src/components/PreventFormTransition/index.js
@@ -0,0 +1,16 @@
+import React from "react"
+import { useFormState } from "react-final-form"
+import { Prompt } from "react-router-dom"
+
+const PreventFormTransition = () => {
+	const formState = useFormState()
+
+	return (
+		<Prompt
+			when={formState.dirty}
+			message={"Zmiany nie zostały zapisane. Czy napewno chcesz wyjść?"}
+		/>
+	)
+}
+
+export default PreventFormTransition
