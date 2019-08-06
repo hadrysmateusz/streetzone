2bc7d4c797a15723286a68e5dd33a09ddb0dd6b5
Mateusz Hadryś <kugot999@gmail.com>
2019-08-06 22:30:29 +0200

    add option to override path in useDeleteDocument

--- src/hooks/useDeleteDocument.js
+++ src/hooks/useDeleteDocument.js
@@ -2,32 +2,36 @@ import { useCallback } from "react"
 import { useFlash, useFirebase } from "."
 
 /**
- * Returns a function that will delete a document in firestore and provides feedback on the results
+ * Returns a function that will delete a document in firestore and provides feedback on the results, a path can be passed to the returned function to override the one set in the hook
  * @param {string} path Slash separated path to the document
  * @param {string} message A message to be displayed in the confirmation prompt, if not provided a generic default will be used
+ * @todo test overriding the path
  */
 const useDeleteDocument = (path, message = "Na pewno? Tej akcji nie da się cofnąć.") => {
 	const flashMessage = useFlash()
 	const firebase = useFirebase()
 
-	const deleteDocument = useCallback(async () => {
-		try {
-			const shouldDelete = window.confirm(message)
+	const deleteDocument = useCallback(
+		async (overridePath) => {
+			try {
+				const shouldDelete = window.confirm(message)
 
-			if (shouldDelete) {
-				await firebase.db.doc(path).delete()
+				if (!shouldDelete) return
+
+				await firebase.db.doc(overridePath || path).delete()
+
+				flashMessage({ type: "success", text: "Usunięto" })
+			} catch (error) {
+				console.error(error)
+				flashMessage({
+					type: "error",
+					text: "Błąd",
+					details: "Więcej informacji w konsoli"
+				})
 			}
-
-			flashMessage({ type: "success", text: "Usunięto" })
-		} catch (error) {
-			console.error(error)
-			flashMessage({
-				type: "error",
-				text: "Błąd",
-				details: "Więcej informacji w konsoli"
-			})
-		}
-	}, [firebase, flashMessage, message, path])
+		},
+		[firebase, flashMessage, message, path]
+	)
 
 	return deleteDocument
 }
