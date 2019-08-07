909b266a80bbe8b9f53445129f43f16334807797
Mateusz Hadryś <kugot999@gmail.com>
2019-08-07 14:32:27 +0200

    transform refinement list and search box into functional comopnents + debounce search

--- src/components/Algolia/AlgoliaRefinementList/SearchBox.js
+++ src/components/Algolia/AlgoliaRefinementList/SearchBox.js
@@ -1,8 +1,37 @@
-import React from "react"
+import React, { useState, useRef } from "react"
 import { Input } from "../../FormElements"
 
-const SearchBox = ({ value, onChange, clear }) => (
-	<Input icon="search" value={value} placeholder="Szukaj" onChange={onChange} />
-)
+const SearchBox = ({ search, debounce = 250 }) => {
+	const [value, setValue] = useState("")
+	const inputRef = useRef()
+	const timeoutRef = useRef(null)
+
+	const onChange = () => {
+		// reset the timeout (debouncing)
+		// don't write the value to a variable, the ref is required
+		clearTimeout(timeoutRef.current)
+
+		// update internal value
+		setValue(inputRef.current.value)
+
+		// set the timeout (debouncing)
+		timeoutRef.current = setTimeout(() => {
+			// trigger facet search
+			search(inputRef.current.value)
+			// remove the timeout (can be used to create a loading state indicator)
+			timeoutRef.current = null
+		}, debounce)
+	}
+
+	return (
+		<Input
+			icon="search"
+			value={value}
+			placeholder="Szukaj"
+			onChange={onChange}
+			ref={inputRef}
+		/>
+	)
+}
 
 export default SearchBox
