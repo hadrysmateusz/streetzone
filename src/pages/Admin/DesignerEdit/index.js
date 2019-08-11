4df4a3b0c902e608a061f13a8dcc133751a758b9
Mateusz Hadryś <kugot999@gmail.com>
2019-08-11 16:08:21 +0200

    remove old designer edit page

--- src/pages/Admin/DesignerEdit/index.js
+++
@@ -1,8 +0,0 @@
-import React from "react"
-import { PageContainer } from "../../../components/Containers"
-
-const DesignerEdit = () => {
-	return <PageContainer>designer edit</PageContainer>
-}
-
-export default DesignerEdit
