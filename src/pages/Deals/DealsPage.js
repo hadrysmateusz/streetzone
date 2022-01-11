f2a056ff20a5eb45dbefdb1a3a28fd573c1b8c95
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2022-01-12 00:23:15 +0100

    increase StatelessSearchWrapper limit on DealsPage

--- src/pages/Deals/DealsPage.js
+++ src/pages/Deals/DealsPage.js
@@ -16,69 +16,65 @@ import { CONST } from "../../constants"
 const sidebarElements = [{ title: "Popularne na blogu", component: PopularArticles }]
 
 const Heading = styled.h1`
-	text-align: center;
-	margin: 0 auto;
-	font-size: var(--fs-xl);
+  text-align: center;
+  margin: 0 auto;
+  font-size: var(--fs-xl);
 `
 
 const Info = styled.p`
-	margin: var(--spacing3) auto var(--spacing4);
-	color: var(--gray0);
-	max-width: 240px;
-	text-align: center;
+  margin: var(--spacing3) auto var(--spacing4);
+  color: var(--gray0);
+  max-width: 240px;
+  text-align: center;
 `
 
 const ResultsContainer = styled.div`
-	margin-top: var(--spacing3);
-	display: grid;
-	gap: var(--spacing3);
-	grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
-	@media (min-width: ${(p) => p.theme.breakpoints[2]}px) {
-		grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
-	}
+  margin-top: var(--spacing3);
+  display: grid;
+  gap: var(--spacing3);
+  grid-template-columns: repeat(auto-fill, minmax(230px, 1fr));
+  @media (min-width: ${(p) => p.theme.breakpoints[2]}px) {
+    grid-template-columns: repeat(auto-fill, minmax(280px, 1fr));
+  }
 `
 
 const DealsPage = () => {
-	const [isAuthenticated] = useAuthentication(true)
+  const [isAuthenticated] = useAuthentication(true)
 
-	return (
-		<StatelessSearchWrapper
-			indexName={CONST.DEALS_ALGOLIA_INDEX}
-			limit={3}
-			ignoreArchivedStatus
-		>
-			<PageContainer>
-				<Heading>Okazje</Heading>
-				<Info>Tu znajdziesz najlepsze deale na ubrania i buty</Info>
+  return (
+    <StatelessSearchWrapper indexName={CONST.DEALS_ALGOLIA_INDEX} limit={8} ignoreArchivedStatus>
+      <PageContainer>
+        <Heading>Okazje</Heading>
+        <Info>Tu znajdziesz najlepsze deale na ubrania i buty</Info>
 
-				<LayoutManager>
-					<Main>
-						<AlgoliaSearchBox
-							placeholder="Szukaj"
-							placeholderLong="Szukaj po nazwie, marce, itd."
-						/>
+        <LayoutManager>
+          <Main>
+            <AlgoliaSearchBox
+              placeholder="Szukaj"
+              placeholderLong="Szukaj po nazwie, marce, itd."
+            />
 
-						<InfiniteScrollingResults>
-							{({ results, hasMore, loadMore }) => (
-								<ResultsContainer>
-									{results.map((deal) => (
-										<BigDealCard {...deal} key={deal.id} />
-									))}
-								</ResultsContainer>
-							)}
-						</InfiniteScrollingResults>
-					</Main>
-					<Sidebar availableElements={sidebarElements} isRandom>
-						{!isAuthenticated && (
-							<BlackBox header="Otrzymuj powiadomienia">
-								Utwórz konto by dostawać powiadomienia o nowych promocjach
-							</BlackBox>
-						)}
-					</Sidebar>
-				</LayoutManager>
-			</PageContainer>
-		</StatelessSearchWrapper>
-	)
+            <InfiniteScrollingResults>
+              {({ results, hasMore, loadMore }) => (
+                <ResultsContainer>
+                  {results.map((deal) => (
+                    <BigDealCard {...deal} key={deal.id} />
+                  ))}
+                </ResultsContainer>
+              )}
+            </InfiniteScrollingResults>
+          </Main>
+          <Sidebar availableElements={sidebarElements} isRandom>
+            {!isAuthenticated && (
+              <BlackBox header="Otrzymuj powiadomienia">
+                Utwórz konto by dostawać powiadomienia o nowych promocjach
+              </BlackBox>
+            )}
+          </Sidebar>
+        </LayoutManager>
+      </PageContainer>
+    </StatelessSearchWrapper>
+  )
 }
 
 export default DealsPage
