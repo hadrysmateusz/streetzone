cfb559093fd48574a36970807fe4e5ff7193ccec
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2022-01-12 00:20:34 +0100

    add two thematic groups to blog home page

--- src/pages/Blog/HomePage/index.js
+++ src/pages/Blog/HomePage/index.js
@@ -2,18 +2,19 @@ import React from "react"
 
 import { PageContainer } from "../../../components/Containers"
 import { StatelessSearchWrapper } from "../../../components/InstantSearchWrapper"
-// import { SmallDropCard, PostCard } from "../../../components/Cards"
-// import { ThematicGroup } from "../../../components/ThematicGroup"
+import { /*  SmallDropCard, */ PostCard } from "../../../components/Cards"
+import { ThematicGroup } from "../../../components/ThematicGroup"
 import { LayoutManager, Main, Sidebar } from "../../../components/LayoutManager"
 import { PoweredByBox } from "../../../components/Algolia/PoweredBy"
 import { PopularArticles, PopularTags } from "../../../components/SidebarComponents"
 
-import { CONST } from "../../../constants"
-// import { route } from "../../../utils"
+import { CONST, POST_CATEGORIES } from "../../../constants"
+import { route } from "../../../utils"
 
 import PromotedSection from "./PromotedSection"
 import CategoryNav from "./CategoryNav"
 import InfinitePosts from "../InfinitePostsList"
+import { TextBlock } from "../../../components/StyledComponents"
 
 const sidebarElements = [
   { title: "Popularne artykuły", component: PopularArticles },
@@ -35,10 +36,30 @@ const sidebarElements = [
 //     index={CONST.BLOG_POST_ALGOLIA_INDEX}
 //     title="Czyszczenie i Pielęgnacja"
 //     linkTo={route("BLOG_TAG", null, { tags: ["Czyszczenie", "Pielęgnacja"] })}
-//     refinements={{ tags: ["Czyszczenie", "Pielęgnacja"] }}
+//     // refinements={{ tags: ["Czyszczenie", "Pielęgnacja"] }}
 //     component={PostCard}
 //   />
 // )
+
+const SneakersGroup = () => (
+  <ThematicGroup
+    index={CONST.BLOG_POST_ALGOLIA_INDEX}
+    title={POST_CATEGORIES.SNEAKERS}
+    linkTo={route("BLOG_CATEGORY", POST_CATEGORIES.SNEAKERS)}
+    refinements={{ category: POST_CATEGORIES.SNEAKERS }}
+    component={PostCard}
+  />
+)
+
+const EventsGroup = () => (
+  <ThematicGroup
+    index={CONST.BLOG_POST_ALGOLIA_INDEX}
+    title={POST_CATEGORIES.EVENT}
+    linkTo={route("BLOG_CATEGORY", POST_CATEGORIES.EVENT)}
+    refinements={{ category: POST_CATEGORIES.EVENT }}
+    component={PostCard}
+  />
+)
 
 const BlogHomePage = () => {
   return (
@@ -52,10 +73,14 @@ const BlogHomePage = () => {
               <CategoryNav />
 
               {/* Preset Groups */}
-              {/* <DropsGroup /> */}
+              <SneakersGroup />
+              <EventsGroup />
               {/* <CareAndMaintenanceGroup /> */}
 
               {/* Infinite Posts List */}
+              <TextBlock bold uppercase size="m" mb="16px">
+                Najnowsze
+              </TextBlock>
               <InfinitePosts />
             </Main>
             <Sidebar availableElements={sidebarElements} isRandom>
