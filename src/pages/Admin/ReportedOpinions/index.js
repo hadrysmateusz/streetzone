fc0b8278af8c2b20faa01cee747d77eb84db3fb6
Mateusz Hadryś <hadrys.mateusz86@gmail.com>
2022-01-19 07:12:35 +0100

    comment out unfinished and unused component

--- src/pages/Admin/ReportedOpinions/index.js
+++ src/pages/Admin/ReportedOpinions/index.js
@@ -1,61 +1,63 @@
-import React from "react"
-import moment from "moment"
-import styled from "styled-components/macro"
+// import React from "react"
+// import moment from "moment"
+// import styled from "styled-components/macro"
 
-import Button, { ButtonContainer } from "../../../components/Button"
+// import Button, { ButtonContainer } from "../../../components/Button"
 
-import { useFirebase, useLiveCollection } from "../../../hooks"
-import LoadingSpinner from "../../../components/LoadingSpinner"
+// import { useDeleteDocument, useLiveCollection } from "../../../hooks"
+// import LoadingSpinner from "../../../components/LoadingSpinner"
 
-const DesignerRequest = styled.div`
-	background: white;
-	border: 1px solid var(--gray75);
-	padding: var(--spacing3);
-	margin-bottom: var(--spacing2);
+// const DesignerRequest = styled.div`
+//   background: white;
+//   border: 1px solid var(--gray75);
+//   padding: var(--spacing3);
+//   margin-bottom: var(--spacing2);
 
-	h3 {
-		margin: 0;
-	}
-`
+//   h3 {
+//     margin: 0;
+//   }
+// `
 
-const OuterContainer = styled.div`
-	margin: var(--spacing4) 0;
-	h2 {
-		margin: 0;
-	}
-`
+// const OuterContainer = styled.div`
+//   margin: var(--spacing4) 0;
+//   h2 {
+//     margin: 0;
+//   }
+// `
 
-const ReportCard = ({}) => {
-	const deleteDocument = useDeleteDocument(`reportedOpinions/${id}`)
+// const ReportCard = ({}) => {
+//   const deleteDocument = useDeleteDocument(`reportedOpinions/${id}`)
 
-	return (
-		<DesignerRequest>
-			<h3>{request.name}</h3>
-			<div>Requested At: {moment(request.requestedAt).format("LL")}</div>
-			<ButtonContainer>
-				<Button danger onClick={deleteDocument}>
-					Usuń
-				</Button>
-			</ButtonContainer>
-		</DesignerRequest>
-	)
-}
+//   return (
+//     <DesignerRequest>
+//       <h3>{request.name}</h3>
+//       <div>Requested At: {moment(request.requestedAt).format("LL")}</div>
+//       <ButtonContainer>
+//         <Button danger onClick={deleteDocument}>
+//           Usuń
+//         </Button>
+//       </ButtonContainer>
+//     </DesignerRequest>
+//   )
+// }
 
-const RequestedDesigners = () => {
-	const { reportedOpinions, isEmpty, isLoading } = useLiveCollection("reportedOpinions")
+// const RequestedDesigners = () => {
+//   const { reportedOpinions, isEmpty, isLoading } = useLiveCollection("reportedOpinions")
 
-	return (
-		<OuterContainer>
-			<h2>Zgłoszone opinie</h2>
-			{isEmpty ? (
-				<div>Brak</div>
-			) : isLoading ? (
-				<LoadingSpinner />
-			) : (
-				reportedOpinions.map((report) => <ReportCard {...report} />)
-			)}
-		</OuterContainer>
-	)
-}
+//   return (
+//     <OuterContainer>
+//       <h2>Zgłoszone opinie</h2>
+//       {isEmpty ? (
+//         <div>Brak</div>
+//       ) : isLoading ? (
+//         <LoadingSpinner />
+//       ) : (
+//         reportedOpinions.map((report) => <ReportCard {...report} />)
+//       )}
+//     </OuterContainer>
+//   )
+// }
 
-export default RequestedDesigners
+// export default RequestedDesigners
+
+export {}
